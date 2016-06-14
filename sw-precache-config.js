c480a65d9242087c8a67b63e2e4650855072b9fd
Marcus Hellberg <marcus@vaadin.com>
2016-06-13 21:06:19 -0700

    precache default receipt

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,8 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/default-receipt.png'
   ],
   navigateFallback: '/index.html'
 };
