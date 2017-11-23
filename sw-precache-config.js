6168e929189f213050820c66ebe59f6c302b35ee
Marcus Hellberg <marcus@vaadin.com>
2017-11-23 13:51:20 -0800

    path fix for prpl server hosting

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,12 +2,10 @@ module.exports = {
   staticFileGlobs: [
     'index.html',
     'manifest.json',
-    '/bower_components/webcomponentsjs/*',
+    'bower_components/webcomponentsjs/*',
     'images/default-receipt.png'
   ],
   navigateFallback: 'index.html',
-  stripPrefix: '',
-  replacePrefix: './',
   runtimeCaching: [{
     urlPattern: /https:\/\/fonts\.googleapis\.com.*/,
     handler: 'cacheFirst'
